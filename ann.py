54dafcd55746787d07167a74f9cc8ee7c8344ab8
First-frost <550991679@qq.com>
2021-06-21 18:38:44 +0800

    fix

--- ann.py
+++ ann.py
@@ -1,39 +1,37 @@
 import tensorflow as tf
 from tensorflow import keras
-
-import functools
+# import other library
+import os
+import gzip
+import numpy as np
 import matplotlib.pyplot as plt
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+from classify import get_data
 
+
+# load train data
+train_images, train_labels = get_data(kind='train')
+
+# load test data
+test_images, test_labels = get_data(kind='t10k')
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
+train_images = train_images.reshape(-1, 28, 28)
+test_images = test_images.reshape(-1, 28, 28)
+
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dropout(0.2),
-    keras.layers.Dense(10)
-])
-print(model.summary())
-
-top2_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=2)
-top2_acc.__name__ = 'top2_acc'
-
-model.compile(optimizer='Adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy', top2_acc])
-
-model.fit(train_images, train_labels, epochs=10)
-
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-
-probability_model = tf.keras.Sequential([
-    model,
-    tf.keras.layers.Softmax()
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(256, activation=tf.nn.relu),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
 
-predictions = probability_model.predict(test_images)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=20)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
