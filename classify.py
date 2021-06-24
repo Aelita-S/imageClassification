331929e5cf48d22ab79bc200bfc1c2eb80e73147
First-frost <550991679@qq.com>
2021-06-24 11:49:13 +0800

    混淆矩阵

--- classify.py
+++ classify.py
@@ -114,6 +114,38 @@ test_images, test_labels = get_data('t10k')
 MODE = ''
 
 
+def plot_confusion_matrix(cm, title='Confusion Matrix'):
+    classes = [i for i in range(len(cm))]
+    plt.figure(figsize=(12, 8), dpi=100)
+    np.set_printoptions(precision=2)
+
+    # 在混淆矩阵中每格的概率值
+    ind_array = np.arange(len(classes))
+    x, y = np.meshgrid(ind_array, ind_array)
+    for x_val, y_val in zip(x.flatten(), y.flatten()):
+        c = cm[y_val][x_val]
+        if c > 0.001:
+            plt.text(x_val, y_val, "%0.2f" % (c,), color='red', fontsize=5, va='center', ha='center')
+
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.binary)
+    plt.title(title)
+    plt.colorbar()
+    plt.ylabel('Actual label')
+    plt.xlabel('Predict label')
+
+    # offset the tick
+    tick_marks = np.array(range(len(classes))) + 0.5
+    plt.gca().set_xticks(tick_marks, minor=True)
+    plt.gca().set_yticks(tick_marks, minor=True)
+    plt.gca().xaxis.set_ticks_position('none')
+    plt.gca().yaxis.set_ticks_position('none')
+    plt.grid(True, which='minor', linestyle='-')
+    plt.gcf().subplots_adjust(bottom=0.15)
+
+    # show confusion matrix
+    plt.show()
+
+
 def run(classification):
     print("算法：", classification['algorithm'].__name__)
     run_train_images = train_images
